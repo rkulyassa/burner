1326c4ba629cbfc1fa88f8efb85e2f0a85fa0dea
Ryan Kulyassa <73004474+rkulyassa@users.noreply.github.com>
2024-10-22 15:41:46 -0400

    Remove model path

--- burner/transcribe.sh
+++ burner/transcribe.sh
@@ -8,7 +8,6 @@ input_file=$1
 
 whisperx \
     --model large-v2 \
-    --model_dir /Users/ryan/Desktop/ShortsCloud/backend/models \
     --compute_type int8 \
     --output_format json \
     --suppress_numerals \
